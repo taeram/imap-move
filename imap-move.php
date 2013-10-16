8e819a96dd19c7129ed516e17d1bbab52ab1a951
Jesse Patching <jesse@patching.ca>
2013-10-16 09:03:11 -0600

    Tidied class docblocks, removed commented out code

--- imap-move.php
+++ imap-move.php
@@ -28,9 +28,9 @@
  *         --target imap-ssl://userB:secret-passwrod@imap.example.com:993/sub-folder \
  *         [ --wipe --fake --copy ]
  *
- *         --fake to just list what would be copied
- *         --wipe to remove messages after they are copied (move)
- *         --copy to store copies of the messages in a path
+ *     --fake to just list what would be copied
+ *     --wipe to remove messages after they are copied (move)
+ *     --copy to store copies of the messages in a path
  */
 
 error_reporting(E_ALL | E_STRICT);
@@ -42,12 +42,8 @@ $S = new IMAP($_ENV['src']);
 
 echo "Connecting Target...\n";
 $T = new IMAP($_ENV['tgt']);
-//$tgt_path_list = $T->listPath();
-//print_r($tgt_path_list);
 
 $src_path_list = $S->listPath();
-// print_r($src_path_list);
-// exit;
 
 foreach ($src_path_list as $path) {
 
@@ -62,7 +58,7 @@ foreach ($src_path_list as $path) {
     // Source Path
     $S->setPath($path['name']);
     $src_path_stat = $S->pathStat();
-    // print_r($src_path_stat);
+
     if (empty($src_path_stat['mail_count'])) {
         echo "S: Skip {$src_path_stat['mail_count']} messages\n";
         continue;
@@ -73,9 +69,11 @@ foreach ($src_path_list as $path) {
     $tgt_path = _path_map($path['name']);
     echo "T: Indexing: $tgt_path\n";
     $T->setPath($tgt_path); // Creates if needed
+
     // Show info on Target
     $tgt_path_stat = $T->pathStat();
     echo "T: {$tgt_path_stat['mail_count']} messages\n";
+
     // Build Index of Target
     $tgt_mail_list = array();
     for ($i=1;$i<=$tgt_path_stat['mail_count'];$i++) {
@@ -83,16 +81,12 @@ foreach ($src_path_list as $path) {
         $tgt_mail_list[ $mail['message_id'] ] = !empty($mail['subject']) ? $mail['subject'] : "[ No Subject ] Message $i";
     }
 
-    // print_r($tgt_mail_list);
-
-    // for ($src_idx=1;$src_idx<=$src_path_stat['mail_count'];$src_idx++) {
     for ($src_idx=$src_path_stat['mail_count'];$src_idx>=1;$src_idx--) {
 
         $stat = $S->mailStat($src_idx);
         $stat['answered'] = trim($stat['Answered']);
         $stat['unseen'] = trim($stat['Unseen']);
         if (empty($stat['subject'])) $stat['subject'] = "[ No Subject ] Message $src_idx";
-        // print_r($stat['message_id']); exit;
 
         if (array_key_exists($stat['message_id'],$tgt_mail_list)) {
             echo "S:$src_idx Mail: {$stat['subject']} Copied Already\n";
@@ -116,7 +110,6 @@ foreach ($src_path_list as $path) {
         $date = strftime('%d-%b-%Y %H:%M:%S +0000',strtotime($stat['MailDate']));
 
         if ($res = $T->mailPut(file_get_contents('mail'),$opts,$date)) {
-            // echo "T: $res\n";
             $S->mailWipe($src_idx);
             echo "{$tgt_path_stat['path']}\n";
         } else {
@@ -133,9 +126,10 @@ class IMAP
     private $_c; // Connection Handle
     private $_c_host; // Server Part {}
     private $_c_base; // Base Path Requested
+
     /**
-        Connect to an IMAP
-    */
+     * Connect to an IMAP
+     */
     function __construct($uri)
     {
         $this->_c = null;
@@ -155,6 +149,7 @@ class IMAP
         $this->_c_host.= '}';
 
         $this->_c_base = $this->_c_host;
+
         // Append Path?
         if (!empty($uri['path'])) {
             $x = ltrim($uri['path'],'/');
@@ -164,13 +159,13 @@ class IMAP
         }
         echo "imap_open($this->_c_host)\n";
         $this->_c = imap_open($this->_c_host,$uri['user'],$uri['pass']);
-        // echo implode(', ',imap_errors());
     }
 
     /**
-        List folders matching pattern
-        @param $pat * == all folders, % == folders at current level
-    */
+     * List folders matching pattern
+     *
+     * @param string $pat If "*", then list all folders, if "%", then list folders at current level
+     */
     function listPath($pat='*')
     {
         $ret = array();
@@ -186,24 +181,19 @@ class IMAP
     }
 
     /**
-        Get a Message
-    */
+     * Get a Message
+     */
     function mailGet($i)
     {
-        // return imap_body($this->_c,$i,FT_PEEK);
         return imap_savebody($this->_c,'mail',$i,null,FT_PEEK);
     }
 
     /**
-        Store a Message with proper date
-    */
+     * Store a Message with proper date
+     */
     function mailPut($mail,$opts,$date)
     {
         $stat = $this->pathStat();
-        // print_r($stat);
-        // $opts = '\\Draft'; // And Others?
-        // $opts = null;
-        // exit;
         $ret = imap_append($this->_c,$stat['check_path'],$mail,$opts,$date);
         if ($buf = imap_errors()) {
             die(print_r($buf,true));
@@ -213,34 +203,30 @@ class IMAP
     }
 
     /**
-        Message Info
-    */
+     * Message Info
+     */
     function mailStat($i)
     {
         $head = imap_headerinfo($this->_c,$i);
         return (array)$head;
-        // $stat = imap_fetch_overview($this->_c,$i);
-        // return (array)$stat[0];
     }
 
     /**
-        Immediately Delete and Expunge the message
-    */
+     * Immediately Delete and Expunge the message
+     */
     function mailWipe($i)
     {
         if ( ($_ENV['wipe']) && (imap_delete($this->_c,$i)) ) return imap_expunge($this->_c);
     }
 
     /**
-        Sets the Current Mailfolder, Creates if Needed
-    */
+     * Sets the Current Mailfolder, Creates if Needed
+     */
     function setPath($p,$make=false)
     {
-        // echo "setPath($p);\n";
         if (substr($p,0,1)!='{') {
             $p = $this->_c_host . trim($p,'/');
         }
-        // echo "setPath($p);\n";
 
         $ret = imap_reopen($this->_c,$p); // Always returns true :(
         $buf = imap_errors();
@@ -264,8 +250,8 @@ class IMAP
     }
 
     /**
-        Returns Information about the current Path
-    */
+     * Returns Information about the current Path
+     */
     function pathStat()
     {
         $res = imap_mailboxmsginfo($this->_c);
@@ -279,14 +265,14 @@ class IMAP
         $ret['check_date'] = $res->Date;
         $ret['check_mail_count'] = $res->Nmsgs;
         $ret['check_path'] = $res->Mailbox;
-        // $ret = array_merge($ret,$res);
+
         return $ret;
     }
 }
 
 /**
-    Process CLI Arguments
-*/
+ * Process CLI Arguments
+ */
 function _args($argc,$argv)
 {
 
@@ -348,8 +334,8 @@ function _args($argc,$argv)
 }
 
 /**
-    @return mapped path name
-*/
+ * @return mapped path name
+ */
 function _path_map($x)
 {
     if (preg_match('/}(.+)$/',$x,$m)) {
@@ -366,8 +352,8 @@ function _path_map($x)
 }
 
 /**
-    @return true if we should skip this path
-*/
+ * @return true if we should skip this path
+ */
 function _path_skip($path)
 {
     if ( ($path['attribute'] & LATT_NOSELECT) == LATT_NOSELECT) {
