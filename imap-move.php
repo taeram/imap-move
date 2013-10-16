2ad3d163f09f2abc64845181eab75113e9a0a87e
Jesse Patching <jesse@patching.ca>
2013-10-16 09:00:31 -0600

    Tidied docblock

--- imap-move.php
+++ imap-move.php
@@ -1,28 +1,37 @@
 #!/usr/bin/php
 <?php
 /**
-    @file
-    @brief Moves Mail from one IMAP account to another
-
-Copyright (C) 2009 Edoceo, Inc
-
-Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-
-Run Like:
-    php ./imap-move.php \
-        --source imap-ssl://userA:secret-password@imap.example.com:993/ \
-        --target imap-ssl://userB:secret-passwrod@imap.example.com:993/sub-folder \
-        [ --wipe --fake --copy ]
-
-    --fake to just list what would be copied
-    --wipe to remove messages after they are copied (move)
-    --copy to store copies of the messages in a path
-
-*/
+ * Moves Mail from one IMAP account to another
+ *
+ * Copyright (C) 2009 Edoceo, Inc
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+ * the Software, and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+ * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+ * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ *
+ * Run Like:
+ *     php ./imap-move.php \
+ *         --source imap-ssl://userA:secret-password@imap.example.com:993/ \
+ *         --target imap-ssl://userB:secret-passwrod@imap.example.com:993/sub-folder \
+ *         [ --wipe --fake --copy ]
+ *
+ *         --fake to just list what would be copied
+ *         --wipe to remove messages after they are copied (move)
+ *         --copy to store copies of the messages in a path
+ */
 
 error_reporting(E_ALL | E_STRICT);
 
